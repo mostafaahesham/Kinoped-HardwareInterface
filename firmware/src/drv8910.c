b9e78d2bb9c137ad162dedb3a086cda2c4dc0deb
Mostafa Hesham <mostafaaheshamm@gmail.com>
2021-07-22 22:46:05 +0200

    added motor brake masks

--- firmware/src/drv8910.c
+++ firmware/src/drv8910.c
@@ -3,7 +3,6 @@
 #include "config/default/peripheral/spi/spi_master/plib_spi3_master.h"
 
 SPI_TRANSFER_SETUP s = {5000000,SPI_CLOCK_PHASE_LEADING_EDGE,SPI_CLOCK_POLARITY_IDLE_LOW,SPI_DATA_BITS_16};
-
 /*--------------------------*/
 /*APIs Implementation*/
 /*--------------------------*/
